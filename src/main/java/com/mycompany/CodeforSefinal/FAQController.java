253df150fdb0cdad47f267b17d3b097b0eb95e3e
SJNextKTQ <theodoresurenian@gmail.com>
2025-04-30 11:02:28 -0400

    FAQ Changes?

--- src/main/java/com/mycompany/CodeforSefinal/FAQController.java
+++ src/main/java/com/mycompany/CodeforSefinal/FAQController.java
@@ -1,26 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.mycompany.CodeforSefinal;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author sonic
- */
 public class FAQController implements Initializable {
+    
+    @FXML private Button returnButton;
+    
+    // Needed imports for JavaFX
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    // FXML Connected method, uses ActionEvent to detect when the button "return" is pressed, when it is pressed it will call the method. 
+    @FXML
+    private void returnToPrimary(ActionEvent event) throws IOException {
+        
+        // Code that will load up the "primary.fxml"
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
+        root = loader.load();
+        PrimaryController primaryController = loader.getController();
+        
+        // Code that loads up the "primary.fxml"
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show(); 
+    }
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
     
 }
