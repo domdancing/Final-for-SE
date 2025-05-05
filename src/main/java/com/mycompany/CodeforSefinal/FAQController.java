011d0a9fd972c34e2c4f8dcba9c4eca2e1368471
SJNextKTQ <theodoresurenian@gmail.com>
2025-05-05 13:05:58 -0400

    FAQController

--- src/main/java/com/mycompany/CodeforSefinal/FAQController.java
+++ src/main/java/com/mycompany/CodeforSefinal/FAQController.java
@@ -1,8 +1,13 @@
 package com.mycompany.CodeforSefinal;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,16 +16,20 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class FAQController implements Initializable {
     
     @FXML private Button returnButton;
+    @FXML private TextArea FAQtxt;
     
     // Needed imports for JavaFX
     private Stage stage;
     private Scene scene;
     private Parent root;
+    
+    private String fileName = "Frequently Asked Questions.txt/";
     
     // FXML Connected method, uses ActionEvent to detect when the button "return" is pressed, when it is pressed it will call the method. 
     @FXML
@@ -37,10 +46,26 @@ public class FAQController implements Initializable {
         stage.setScene(scene);
         stage.show(); 
     }
+    
+    public ArrayList<String> openFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader((fileName)));
+        String line;
+        ArrayList<String> content = new ArrayList<>();
+        while((line = reader.readLine()) != null){
+            content.add(line);
+        }
+        return content;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            for (String line : openFile()) {
+                FAQtxt.appendText(line + "\n");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(FAQController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
